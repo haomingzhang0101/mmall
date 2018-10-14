df4402101c20e4c8cdd3cf5d2c4c4ec5f9643d29
haomingzhang0101 <haomingzhang0101@gmail.com>
2018-10-13 20:18:18 -0700

    Add the order module.

--- src/main/java/com/haoming/common/Const.java
+++ src/main/java/com/haoming/common/Const.java
@@ -46,4 +46,89 @@ public class Const {
             return code;
         }
     }
+
+    public enum OrderStatusEnum{
+        CANCELED(0, "CANCELED"),
+        NO_PAY(10, "NO_PAY"),
+        PAID(20, "PAID"),
+        SHIPPED(40, "SHIPPED"),
+        ORDER_SUCCESS (50, "ORDER_SUCCESS"),
+        ORDER_CLOSE(60, "ORDER_CLOSE");
+
+        OrderStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+        public int getCode() {
+            return code;
+        }
+
+        public static OrderStatusEnum codeOf(int code) {
+            for (OrderStatusEnum orderStatusEnum : values()) {
+                if (orderStatusEnum.getCode() == code) {
+                    return orderStatusEnum;
+                }
+            }
+            throw new RuntimeException("Enumerate not found");
+        }
+    }
+
+    public interface AlipayCallback{
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+    }
+
+    public enum PayPlatformEnum {
+        ALIPAY(1, "ALIPAY")
+        ;
+        PayPlatformEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public enum PaymentTypeEnum {
+        ONLINE_PAY(1, "ONLINE_PAY")
+        ;
+        PaymentTypeEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+        public int getCode() {
+            return code;
+        }
+
+        public static PaymentTypeEnum codeOf(int code) {
+            for (PaymentTypeEnum paymentTypeEnum : values()) {
+                if (paymentTypeEnum.getCode() == code) {
+                    return paymentTypeEnum;
+                }
+            }
+            throw new RuntimeException("Enumerate not found");
+        }
+    }
 }
---
+++ src/main/java/com/haoming/controller/backend/OrderManageController.java
@@ -0,0 +1,87 @@
+package com.haoming.controller.backend;
+
+import com.haoming.common.Const;
+import com.haoming.common.ResponseCode;
+import com.haoming.common.ServerResponse;
+import com.haoming.dao.OrderMapper;
+import com.haoming.pojo.Order;
+import com.haoming.pojo.User;
+import com.haoming.service.IOrderService;
+import com.haoming.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/manage/order")
+public class OrderManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private IOrderService iOrderService;
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse orderList(HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "Please login first");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iOrderService.manageList(pageNum, pageSize);
+        } else {
+            return ServerResponse.createByErrorMessage("You don't have the permission to perform this operation.");
+        }
+    }
+
+    @RequestMapping("detail.do")
+    @ResponseBody
+    public ServerResponse orderDetail(HttpSession session, Long orderNo){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "Please login first");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iOrderService.manageDetail(orderNo);
+        } else {
+            return ServerResponse.createByErrorMessage("You don't have the permission to perform this operation.");
+        }
+    }
+
+    @RequestMapping("search.do")
+    @ResponseBody
+    public ServerResponse orderSearch(HttpSession session, Long orderNo, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "Please login first");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iOrderService.manageSearch(orderNo, pageNum, pageSize);
+        } else {
+            return ServerResponse.createByErrorMessage("You don't have the permission to perform this operation.");
+        }
+    }
+
+    @RequestMapping("send_goods.do")
+    @ResponseBody
+    public ServerResponse orderSendGoods(HttpSession session, Long orderNo){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "Please login first");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iOrderService.manageSendGoods(orderNo);
+        } else {
+            return ServerResponse.createByErrorMessage("You don't have the permission to perform this operation.");
+        }
+    }
+
+
+}
